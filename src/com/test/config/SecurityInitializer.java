ef2e1b47898897880ba220312f49c3b1b8434ce8
roger <raismaelr@gmail.com>
2018-10-23 06:47:21 -0400

    se mueve la app al paquete com.empresa

--- src/com/test/config/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.test.config;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-} 
--- src/com/test/model/security/AuthorityName.java
+++
@@ -1,5 +0,0 @@
-package com.test.model.security;
-
-public enum AuthorityName {
-    ROLE_USER, ROLE_ADMIN
-}
